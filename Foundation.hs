b8891d59282b07a4ddb636866955a54f57941e09
Petter Rasmussen <petter.rasmussen@gmail.com>
2015-07-20 21:13:52 +0200

    Increase cookie expiration time

--- Foundation.hs
+++ Foundation.hs
@@ -61,7 +61,7 @@ instance Yesod App where
     -- Store session data on the client in encrypted cookies,
     -- default session idle timeout is 120 minutes
     makeSessionBackend _ = fmap Just $ defaultClientSessionBackend
-        10080    -- timeout in minutes
+        525600    -- timeout in minutes
         "config/client_session_key.aes"
 
     defaultLayout widget = do
