62b4e95eef72186b66531115f1f6332fa01d3b39
Petter Rasmussen <petter.rasmussen@gmail.com>
2016-05-25 22:11:04 +0200

    Add full date to tooltip

--- static/js/date-format.js
+++ static/js/date-format.js
@@ -1,8 +1,17 @@
 (function() {
     $("[data-iso-date]").each(function(i, elem) {
-        var elem = $(elem);
-        var isoDate = elem.data("iso-date");
-        var relDate = moment(isoDate).fromNow();
-        elem.find(".formated-date").text(relDate);
+        var srcElem = $(elem);
+        var dstElem = srcElem.find(".formated-date");
+        var isoDate = srcElem.data("iso-date");
+        var date = moment(isoDate);
+
+        dstElem.text(date.fromNow());
+        dstElem.tooltip({
+            title: date.format("LLL"),
+            delay: {
+                "show": 500,
+                "hide": 100,
+            },
+        });
     });
 })();
