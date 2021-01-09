fc4a59784e227e23e87ff5fe9867c04016e70505
Petter Rasmussen <petter@hask.no>
2021-01-09 11:09:10 +0100

    Create generic function for getting entities with count

--- Handler/Snippets.hs
+++ Handler/Snippets.hs
@@ -6,14 +6,220 @@ import Widget.Pagination (paginationWidget)
 import Database.Persist.Sql
 import Data.Function ((&))
 import qualified Model.Pagination as Pagination
+import Util.Multiline (multiline)
 
 
-snippetsPerPage :: Int
-snippetsPerPage = 20
+
+getSnippetsR :: Handler Html
+getSnippetsR = do
+    currentPage <- pageNo <$> lookupGetParam "page"
+    maybeLanguageParam <- lookupGetParam "language"
+    let snippetsPerPage = 20
+    let limitOffset = LimitOffset
+            { limit = snippetsPerPage
+            , offset = (currentPage - 1) * snippetsPerPage
+            }
+    entities <- getEntitiesWithCount (getEntitiesQuery limitOffset maybeLanguageParam)
+    let SnippetEntriesWithPagination{..} = toSnippetsWithPagination currentPage snippetsPerPage entities
+    defaultLayout $ do
+        setTitle $ title "Public snippets"
+        setDescription "List of public code snippets"
+        addScript $ StaticR js_date_js
+        $(widgetFile "snippets")
 
 
-toPagination :: Int -> Int -> Pagination
-toPagination currentPage totalPages =
+getEntitiesQuery :: LimitOffset -> Maybe Text -> GetEntitiesWithCountQuery
+getEntitiesQuery limitOffset maybeLanguage =
+    case maybeLanguage of
+        Just language ->
+            getSnippetsByLanguageWithCountQuery language limitOffset
+
+        Nothing ->
+            getSnippetsWithCountQuery limitOffset
+
+
+toSnippetsWithPagination :: Int -> Int -> EntitiesWithCount (Entity CodeSnippet, Maybe (Entity Profile)) -> SnippetEntriesWithPagination
+toSnippetsWithPagination currentPage perPage EntitiesWithCount{..} =
+    SnippetEntriesWithPagination
+        { entries = map (uncurry snippetEntryFromEntity) entities
+        , pagination = paginationFromPageData
+            PageData
+                { currentPage = currentPage
+                , totalPages = totalPagesFromCount entitiesCount perPage
+                }
+        }
+
+
+
+data GetEntitiesWithCountQuery = GetEntitiesWithCountQuery
+    { getEntities :: RawQuery
+    , countEntities :: RawQuery
+    }
+
+data EntitiesWithCount a = EntitiesWithCount
+    { entities :: [a]
+    , entitiesCount :: Int64
+    }
+
+getEntitiesWithCount :: RawSql a => GetEntitiesWithCountQuery -> Handler (EntitiesWithCount a)
+getEntitiesWithCount GetEntitiesWithCountQuery{..} = do
+    (entities, entitiesCount) <- runDB $ do
+        entities <- rawSql (query getEntities) (queryValues getEntities)
+        entityCount <- rawSql (query countEntities) (queryValues countEntities)
+        pure (entities, entityCount)
+    pure $ EntitiesWithCount
+        { entities = entities
+        , entitiesCount =
+            entitiesCount
+                & listToMaybe
+                & fmap unSingle
+                & fromMaybe 0
+        }
+
+
+data LimitOffset = LimitOffset
+    { limit :: Int
+    , offset :: Int
+    }
+
+getSnippetsWithCountQuery :: LimitOffset -> GetEntitiesWithCountQuery
+getSnippetsWithCountQuery LimitOffset{..} =
+    GetEntitiesWithCountQuery
+        { getEntities = RawQuery
+            { query = [multiline|
+                select
+                    ??, ??
+                from
+                    code_snippet
+                left join
+                    profile on code_snippet.user_id = profile.user_id
+                where
+                    code_snippet.public is true
+                and
+                    code_snippet.language <> 'plaintext'
+                and
+                    lower(code_snippet.title) <> 'untitled'
+                order by
+                    code_snippet.created desc
+                limit ?
+                offset ?
+                |]
+
+            , queryValues =
+                [ toPersistValue limit
+                , toPersistValue offset
+                ]
+            }
+        , countEntities = RawQuery
+            { query = [multiline|
+                select
+                    count(*)
+                from
+                    code_snippet
+                where
+                    code_snippet.public is true
+                and
+                    code_snippet.language <> 'plaintext'
+                and
+                    lower(code_snippet.title) <> 'untitled'
+                |]
+
+            , queryValues = []
+            }
+        }
+
+
+getSnippetsByLanguageWithCountQuery :: Text -> LimitOffset -> GetEntitiesWithCountQuery
+getSnippetsByLanguageWithCountQuery language LimitOffset{..} =
+    GetEntitiesWithCountQuery
+        { getEntities = RawQuery
+            { query = [multiline|
+                select
+                    ??, ??
+                from
+                    code_snippet
+                left join
+                    profile on code_snippet.user_id = profile.user_id
+                where
+                    code_snippet.public is true
+                and
+                    code_snippet.language = ?
+                and
+                    lower(code_snippet.title) <> 'untitled'
+                order by
+                    code_snippet.created desc
+                limit ?
+                offset ?
+                |]
+
+            , queryValues =
+                [ toPersistValue language
+                , toPersistValue limit
+                , toPersistValue offset
+                ]
+            }
+        , countEntities = RawQuery
+            { query = [multiline|
+                select
+                    count(*)
+                from
+                    code_snippet
+                where
+                    code_snippet.public is true
+                and
+                    code_snippet.language = ?
+                and
+                    lower(code_snippet.title) <> 'untitled'
+                |]
+
+            , queryValues =
+                [ toPersistValue language
+                ]
+            }
+        }
+
+
+data SnippetEntry = SnippetEntry
+    { entrySnippet :: CodeSnippet
+    , entryProfile :: Maybe Profile
+    }
+
+
+snippetEntryFromEntity :: Entity CodeSnippet -> Maybe (Entity Profile) -> SnippetEntry
+snippetEntryFromEntity codeSnippetEntity profileEntity =
+    SnippetEntry
+        { entrySnippet = entityVal codeSnippetEntity
+        , entryProfile = fmap entityVal profileEntity
+        }
+
+
+entryTitle :: SnippetEntry -> Text
+entryTitle SnippetEntry{..} =
+    codeSnippetTitle entrySnippet
+        & take 50
+
+
+
+data RawQuery = RawQuery
+    { query :: Text
+    , queryValues :: [PersistValue]
+    }
+
+
+
+data SnippetEntriesWithPagination = SnippetEntriesWithPagination
+    { entries :: [SnippetEntry]
+    , pagination :: Pagination
+    }
+
+
+data PageData = PageData
+    { currentPage :: Int
+    , totalPages :: Int
+    }
+
+paginationFromPageData :: PageData -> Pagination
+paginationFromPageData PageData{..} =
     if currentPage > totalPages then
         Pagination.Pagination
             { Pagination.paginationNextPage = Nothing
@@ -63,78 +269,3 @@ totalPagesFromCount totalCount perPage =
 intToText :: Int -> Text
 intToText n =
     pack (show n)
-
-
-getSnippetsR :: Handler Html
-getSnippetsR = do
-    currentPage <- pageNo <$> lookupGetParam "page"
-    maybeLanguage <- lookupGetParam "language"
-    (snippets, pagination) <- case maybeLanguage of
-        Just language -> do
-            snippets <- getSnippetsByLanguage language currentPage
-            snippetsCount <- countSnippetsByLanguage language
-            pure
-                ( snippets
-                , toPagination currentPage (totalPagesFromCount snippetsCount snippetsPerPage)
-                )
-
-        Nothing -> do
-            snippets <- getSnippets currentPage
-            snippetsCount <- countSnippets
-            pure
-                ( snippets
-                , toPagination currentPage (totalPagesFromCount snippetsCount snippetsPerPage)
-                )
-    defaultLayout $ do
-        setTitle $ title "Public snippets"
-        setDescription "List of public code snippets"
-        addScript $ StaticR js_date_js
-        $(widgetFile "snippets")
-
-
-getSnippets :: Int -> Handler [(CodeSnippet, Maybe Profile)]
-getSnippets currentPage =
-    let
-        offset =
-            (currentPage - 1) * snippetsPerPage
-
-        toValues (codeSnippetEntity, profileEntity) =
-            (entityVal codeSnippetEntity, fmap entityVal profileEntity)
-    in do
-    entities <- runDB $ rawSql "select ??, ?? from code_snippet left join profile on code_snippet.user_id = profile.user_id where code_snippet.public is true and code_snippet.language <> 'plaintext' and lower(code_snippet.title) <> 'untitled' order by code_snippet.created desc limit ? offset ?" [toPersistValue snippetsPerPage, toPersistValue offset]
-    entities
-        & map toValues
-        & pure
-
-
-countSnippets :: Handler Int64
-countSnippets = do
-    rows <- runDB $ rawSql "select count(*) from code_snippet where code_snippet.public is true and code_snippet.language <> 'plaintext' and lower(code_snippet.title) <> 'untitled'" []
-    listToMaybe rows
-        & fmap unSingle
-        & fromMaybe 0
-        & pure
-
-
-getSnippetsByLanguage :: Text -> Int -> Handler [(CodeSnippet, Maybe Profile)]
-getSnippetsByLanguage language currentPage =
-    let
-        offset =
-            (currentPage - 1) * snippetsPerPage
-
-        toValues (codeSnippetEntity, profileEntity) =
-            (entityVal codeSnippetEntity, fmap entityVal profileEntity)
-    in do
-    entities <- runDB $ rawSql "select ??, ?? from code_snippet left join profile on code_snippet.user_id = profile.user_id where code_snippet.public is true and code_snippet.language = ? and lower(code_snippet.title) <> 'untitled' order by code_snippet.created desc limit ? offset ?" [toPersistValue language, toPersistValue snippetsPerPage, toPersistValue offset]
-    entities
-        & map toValues
-        & pure
-
-
-countSnippetsByLanguage :: Text -> Handler Int64
-countSnippetsByLanguage language = do
-    rows <- runDB $ rawSql "select count(*) from code_snippet where code_snippet.public is true and code_snippet.language = ? and lower(code_snippet.title) <> 'untitled'" [PersistText language]
-    listToMaybe rows
-        & fmap unSingle
-        & fromMaybe 0
-        & pure
