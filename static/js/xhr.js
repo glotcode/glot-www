8e5a7a921e318bf0de783b85575b327c32dfcba3
Petter Rasmussen <petter@hask.no>
2021-03-27 20:27:48 +0100

    Upgrade to newest slim version of jquery

--- static/js/xhr.js
+++ static/js/xhr.js
@@ -1,29 +1,58 @@
-window.XHR = {
-  delete: function(url) {
-    return $.ajax({
-      type: "DELETE",
-      url: url,
-      dataType: "json",
-    });
-  },
+(function() {
 
-  jsonPut: function(url, data) {
-    return $.ajax({
-      type: "PUT",
-      url: url,
-      data: JSON.stringify(data),
-      contentType: "application/json",
-      dataType: "json",
-    });
-  },
+    function sendJson(url, method, data) {
+        const options = {
+            method: method,
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        };
 
-  jsonPost: function(url, data) {
-    return $.ajax({
-      type: "POST",
-      url: url,
-      data: JSON.stringify(data),
-      contentType: "application/json",
-      dataType: "json",
-    });
-  },
-};
+        return fetch(url, options).then((response) => {
+            if (!response.ok) {
+                return getErrorMessage(response).then((msg) => Promise.reject(msg));
+            }
+
+            return response.json();
+        });
+    }
+
+    function getErrorMessage(response) {
+        if (isJsonResponse(response)) {
+            return response.json().then((data) => data.message);
+        } else {
+            return response.text();
+        }
+    }
+
+    function isJsonResponse(response) {
+        const contentType = response.headers.get('content-type');
+        return contentType && contentType.includes('application/json');
+    }
+
+    window.XHR = {
+        delete: function(url) {
+            const options = {
+                method: 'DELETE',
+            };
+
+            return fetch(url, options).then((response) => {
+                if (!response.ok) {
+                    return Promise.reject(response);
+                }
+
+                return null;
+            });
+        },
+
+        jsonPut: function(url, data) {
+            return sendJson(url, 'PUT', data);
+        },
+
+        jsonPost: function(url, data) {
+            return sendJson(url, 'POST', data);
+        },
+    };
+
+})();
